9bb6cde565aa6884bd1353ba4c9d29d8510eb980
premshetgaonkar1 <premshetgaonkar1@gmail.com>
2025-02-08 22:30:45 +0530

    add item controller and initialize registerItems function

---
+++ src/controllers/item.controller.js
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+
+const prisma = new PrismaClient()
+
+const registerItems= asyncHandler(async(req,res)=>{
+    
+})
+
